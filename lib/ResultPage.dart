9f76cb04859e6277591c8920b791b46bfb9aba45
viraj-it <vkpatva1150@gmail.com>
2021-04-11 22:18:50 +0530

    Updated Android Manifest

--- lib/ResultPage.dart
+++ lib/ResultPage.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
 import 'List.dart';
 
 class ResultPage extends StatelessWidget {
